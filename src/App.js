30cf4199754063c2445e2bc7a814cb74ac648e02
ndawinata <nanda.winata@gmail.com>
2020-09-11 22:42:22 +0700

    Sign In

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import Splash from './Pages/Splash/Splash'
+import SignIn from './Pages/SignIn/SignIn'
 
 export class App extends Component {
   render() {
     return (
-      <Splash />
+      // <Splash />
+      <SignIn />
     )
   }
 }
---
+++ src/Pages/SignIn/SignIn.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import { Text, View, Image, StatusBar, Dimensions, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import Logo from '../../Assets/icons/Logo.png'
+
+const windowWidth = Dimensions.get('window').width;
+
+export class SignIn extends Component {
+
+    state = {
+        email:"",
+        password:"",
+        val:0
+    }
+
+    render() {
+        return (
+            <View style={{flex:1, backgroundColor:'#334753', resizeMode:'cover', justifyContent:"center"}} >
+                <StatusBar hidden={true} />
+                <View style={{justifyContent:'center', alignItems:'center'}} >
+                    <Image source={Logo} style={{width:190, height:96, marginLeft:50, marginBottom:50}} />
+                    <TextInput 
+                        style={style.email} 
+                        onChangeText = {text => this.setState({...this.state, email:text})} 
+                        placeholder="Email"
+                        underlineColorAndroid = "#30BBBA" 
+                        placeholderTextColor="#30BBBA"
+                        selectionColor="#30BBBA"
+                        keyboardType="email-address"
+                        placeholderTextColor="#30BBBA"
+                        autoCompleteType="email"
+                        value={this.state.email} />
+                    <TextInput 
+                        style={style.password} 
+                        onChangeText = {text => this.setState({...this.state, password:text})} 
+                        secureTextEntry={true}
+                        underlineColorAndroid = "#30BBBA" 
+                        placeholderTextColor="#30BBBA"
+                        selectionColor="#30BBBA"
+                        placeholderTextColor="#30BBBA"
+                        placeholder="Password"
+                        value={this.state.password} />
+                    <TouchableOpacity 
+                        style={style.signin}
+                        onPress={c=>this.setState({...this.state, val:c})}
+                        >
+                        <Text style={{fontSize:20}}>Sign In</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    email:{
+        fontSize:18, 
+        height:40, 
+        width:windowWidth*0.65, 
+        color:'#fff', 
+        marginVertical:10, 
+        paddingHorizontal:10
+    },
+    password:{
+        fontSize:18, 
+        height:40, 
+        width:windowWidth*0.65, 
+        color:'#fff', 
+        marginVertical:10, 
+        paddingHorizontal:10
+    },
+    signin:{
+        borderRadius:8,
+        justifyContent:'center', 
+        alignItems:'center', 
+        height:40, 
+        width:windowWidth*0.65, 
+        backgroundColor:'#30BBBA',
+        marginVertical:10, 
+        marginVertical:15
+    }
+})
+
+export default SignIn
